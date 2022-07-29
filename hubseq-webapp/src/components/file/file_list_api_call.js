c88ae91a6a375eb1ff83de9597424f018254491b
Jerry Chen <jerry@Jerrys-MacBook-Air.local>
2022-07-29 13:49:51 -0700

    moved response formatting stuff to a function

--- hubseq-webapp/src/components/file/file_list_api_call.js
+++ hubseq-webapp/src/components/file/file_list_api_call.js
@@ -9,6 +9,29 @@ const client = axios.create({
   method: 'POST'
 });
 
+const _addKeys = function(e, idx){
+  e["id"] = idx;
+  e["Key"] = e["Prefix"] // .split('/').pop();
+  e["LastModified"] = "-";
+  e["Size"] = 0;
+  return e
+}
+
+const formatResponse_FileList = function( response_raw ){
+  let response = {"data": []};
+  let num_folders = 0;
+
+  num_folders = response_raw.data.hasOwnProperty("CommonPrefixes") ? response_raw.data["CommonPrefixes"].length : 0;
+  // add a few keys to raw response
+  response_raw.data["CommonPrefixes"].forEach((e, idx) => _addKeys(e, idx));
+  response_raw.data["Contents"].forEach((e, idx) => e["id"] = num_folders+idx);
+  // add folders to final response data first
+  response.data = response_raw.data.hasOwnProperty("CommonPrefixes") ? response_raw.data["CommonPrefixes"] : [];
+  // then add files
+  response.data = response.data.concat( response_raw.data["Contents"] );
+  return response
+}
+
 export default function FileList() {
   const [file, setFile] = React.useState([]);
   const [path, setPath] = React.useState()
@@ -17,25 +40,10 @@ export default function FileList() {
       const body = {"path": "s3://www.hubseq.com/assets/"};
       const response_raw = await client.request({"data": body});
       console.log(response_raw);
-      const _addKeys = function(e, idx){
-        e["id"] = idx;
-        e["Key"] = e["Prefix"] // .split('/').pop();
-        e["LastModified"] = "-";
-        e["Size"] = 0;
-        return e
-      }
-      let num_folders = 0;
-      let response = {"data": []}
 
-      num_folders = response_raw.data.hasOwnProperty("CommonPrefixes") ? response_raw.data["CommonPrefixes"].length : 0;
-      // add a few keys to raw response
-      response_raw.data["CommonPrefixes"].forEach((e, idx) => _addKeys(e, idx));
-      response_raw.data["Contents"].forEach((e, idx) => e["id"] = num_folders+idx);
-      // add folders to final response data first
-      response.data = response_raw.data.hasOwnProperty("CommonPrefixes") ? response_raw.data["CommonPrefixes"] : [];
-      // then add files
-      response.data = response.data.concat( response_raw.data["Contents"] );
+      const response = formatResponse_FileList(response_raw)
       console.log(response);
+
       setFile(response.data);
       // setFile([response.data]);
     }
