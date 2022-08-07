a60c05e23888e39215d55de61a92828d4186dca7
Jerry Chen <jerry@Jerrys-MacBook-Air.local>
2022-08-06 22:13:55 -0700

    added expressionQC DEQC GOQC to Run Reports.

--- hubseq-webapp/src/components/run/run-report-modal.js
+++ hubseq-webapp/src/components/run/run-report-modal.js
@@ -17,23 +17,41 @@ import * as path from 'path';
 // assumes that runs are in [HOMEDIR]/runs/[runid]/...
 export const RunReportModal = ({runsSelected, runInfo, props}) => {
     const [open, setOpen] = useState(false);
-    const [reportFiles, setReportFiles] = useState([]);
+    const [reportFilesFastQC, setReportFilesFastQC] = useState([]);
+    const [reportFilesExpressionQC, setReportFilesExpressionQC] = useState([]);
+    const [reportFilesDEQC, setReportFilesDEQC] = useState([]);
+    const [reportFilesGOQC, setReportFilesGOQC] = useState([]);
+
     const baseRunPath = "hubtenants/tranquis/runs/";
 
     let run_report_button;
     let runs_array = runInfo.map(d => d["runid"]);
 
     React.useEffect(() => {
-      async function getReports() {
+      async function getReports(reportType) {
+        let htmlFiles = [];
         console.log('REPORT RUNS SELECTED: ', runsSelected);
         console.log('REPORT RUNS INFO: ', runInfo);
-        console.log('the file call: ', path.join(baseRunPath,runInfo[runsSelected]['runid'],'fastqc'));
-        const htmlFiles = await getFileCall(path.join(baseRunPath,runInfo[runsSelected]['runid'],'fastqc'), ".html");
-        console.log('HTML FILES! ', htmlFiles);
-        setReportFiles(htmlFiles);
+        // console.log('the file call: ', path.join(baseRunPath,runInfo[runsSelected]['runid'],'fastqc'));
+        if (reportType == "FastQC"){
+          htmlFiles = await getFileCall(path.join(baseRunPath,runInfo[runsSelected]['runid'],'fastqc'), ".html");
+          setReportFilesFastQC(htmlFiles);
+        } else if (reportType == "ExpressionQC"){
+          htmlFiles = await getFileCall(path.join(baseRunPath,runInfo[runsSelected]['runid'],'expressionqc'), ".html");
+          setReportFilesExpressionQC(htmlFiles);
+        } else if (reportType == "DEQC"){
+          htmlFiles = await getFileCall(path.join(baseRunPath,runInfo[runsSelected]['runid'],'deqc'), ".html");
+          setReportFilesDEQC(htmlFiles);
+        } else if (reportType == "GOQC"){
+          htmlFiles = await getFileCall(path.join(baseRunPath,runInfo[runsSelected]['runid'],'goqc'), ".html");
+          setReportFilesGOQC(htmlFiles);
+        }
       }
       if (runsSelected){
-        getReports();
+        getReports("FastQC");
+        getReports("ExpressionQC");
+        getReports("DEQC");
+        getReports("GOQC");
       }
     }, [runsSelected]);
 
@@ -52,6 +70,7 @@ export const RunReportModal = ({runsSelected, runInfo, props}) => {
                           onClick={handleClickOpen}> Run Report
                   </Button>
 
+    // report is specific to RNA-Seq - will generalize this modal later
     return (
         <>
         <Button startIcon={(<UploadIcon fontSize="small" />)}
@@ -62,7 +81,10 @@ export const RunReportModal = ({runsSelected, runInfo, props}) => {
         <DialogTitle>Run Report for {runInfo[runsSelected]['runid']}</DialogTitle>
         <DialogContent>
           <Box sx={{ mt: 3 }}>
-            <ReportTable title="FASTQC Reports" filelist={reportFiles} filetype="FastQC" />
+            <ReportTable title="FASTQC Reports" filelist={reportFilesFastQC} filetype="FastQC" />
+            <ReportTable title="Gene Expression QC Reports" filelist={reportFilesExpressionQC} filetype="ExpressionQC" />
+            <ReportTable title="Differential Expression QC Reports" filelist={reportFilesDEQC} filetype="DEQC" />
+            <ReportTable title="Gene Ontology QC Reports" filelist={reportFilesGOQC} filetype="GOQC" />
           </Box>
         </DialogContent>
         <DialogActions>
