b23b95a7e693d6f9c239b01325527399ca692738
gdcorrea <gdcorrea7@gmail.com>
2022-08-21 22:38:13 -0700

    Add jwt callbacks to nextauth

--- hubseq-webapp/src/pages/api/auth/[...nextauth].js
+++ hubseq-webapp/src/pages/api/auth/[...nextauth].js
@@ -2,8 +2,9 @@ import NextAuth from "next-auth"
 import CognitoProvider from "next-auth/providers/cognito"
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  debug: true,
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     CognitoProvider({
         clientId: process.env.COGNITO_CLIENT_ID,
@@ -12,4 +13,18 @@ export default NextAuth({
       })
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.idToken = account.id_token
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.idToken = token.idToken
+      return session
+    }
+  }
 })
